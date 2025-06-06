3243a3fbbec8fcde8f1255dcfd3a79e5d955185e
varunxsensei <varunxai27@gmail.com>
2025-06-06 12:05:15 +0530

    added json decoding func

--- Weather App/ContentView.swift
+++ Weather App/ContentView.swift
@@ -6,16 +6,70 @@
 //
 
 import SwiftUI
+import CoreLocation
 
 struct ContentView: View {
+    @StateObject private var locationManager = LocationManager()
+    @State private var weatherData : WeatherData?
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            if let weatherData = weatherData {
+                Text("\(Int(weatherData.temperature))°C")
+                    .font(.title)
+                    .padding()
+                
+                VStack {
+                    Text("\(weatherData.locationName )")
+                        .font(.title2).bold()
+                        .padding()
+                    Text("\(weatherData.condition)")
+                        .font(.body).bold()
+                    
+                }
+                Spacer()
+                
+            }
+            else{
+                ProgressView()
+            }
         }
-        .padding()
+        .frame(width: 300,height: 300)
+        .background(.ultraThinMaterial)
+        .cornerRadius(20)
+        .onAppear{
+            locationManager.requestLocation()
+        }
+        .onReceive(locationManager.$location){ location in
+            guard let location = location else {return}
+            fetchWeatherData(for: location)
+        }
+    }
+    
+    private func fetchWeatherData(for location : CLLocation){
+        let apiKey = "f147fccc9f1a888232756bfc1d53e28d"
+        let urlString = "https://api.openweathermap.org/data/2.5/weather?lat=\(location.coordinate.latitude)&lon=\(location.coordinate.longitude)&units=metric&appid=\(apiKey)"
+        
+        guard let url = URL(string : urlString) else{return}
+        URLSession.shared.dataTask(with: url){
+            data, _ ,error in
+            guard let data = data else {
+                return
+            }
+            do {
+                let decoder = JSONDecoder()
+                let weatherResponse = try decoder.decode(WeatherResponse.self, from: data)
+                
+                DispatchQueue.main.async {
+                    weatherData = WeatherData(locationName:
+                    weatherResponse.name,temperature:weatherResponse.main.temp,condition:
+                        weatherResponse.weather.first?.description ?? "")
+                }
+            }
+            
+            catch{
+                print(error.localizedDescription)
+            }
+        }.resume()
     }
 }
 
